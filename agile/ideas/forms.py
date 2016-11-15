55dde4fc825806ede637b8a004de4194de49e3c4
Gorka Revilla <gorkarevilla@gmail.com>
2016-11-15 19:04:38 +0100

    Clean compile errors

--- agile/ideas/forms.py
+++ agile/ideas/forms.py
@@ -1,6 +1,10 @@
 from django import forms
+from django.contrib.auth.models import User
+from .models import Comment
 
 # Comment Ideas Form
+
+
 class CommentForm(forms.ModelForm):
 	class Meta:
 		model = Comment
--- agile/ideas/models.py
+++ agile/ideas/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# Comment Ideas Model 
+class Comment(models.Model):
+    idea_id= models.ForeignKey('Ideas',db_column='idea_id', blank=False, null=False, related_name='comments')
+    user_name = models.CharField(max_length=80)
+    comment = models.TextField()
